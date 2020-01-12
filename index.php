ba0bbe3d9a04fa558d3516fc8399e5329498c7fc
Cody Zipp <Czipp2@mail.greenriver.edu>
2020-01-12 18:51:12 -0500

    added an index page

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cupcake</title>
+</head>
+<body>
+<h1>Cupcake Fundraiser</h1>
+    <form>
+        <label for="name">Your Name:</label><br>
+        <input name="name" id="name" type="text" placeholder="Please input your name."><br>
+        <label for="options">Cupcake flavors:</label><br>
+        <input type="checkbox" name="options" value="">The Grasshopper<br>
+        <input type="checkbox" name="options" value="">Whisky Maple Bacon<br>
+        <input type="checkbox" name="options" value="">Carrot Walnut<br>
+        <input type="checkbox" name="options" value="">Salted Caramel Cupcake<br>
+        <input type="checkbox" name="options" value="">Red Velvet<br>
+        <input type="checkbox" name="options" value="">Lemon Drop<br>
+        <input type="checkbox" name="options" value="">Tiramisu<br>
+        <button type="submit">Order</button>
+<!--        --><?php
+//            $flavors = array("grasshopper" => "The Grasshopper", 'maple' => 'Whiskey Maple Bacon',
+//                "carrot" => "Carrot Walnut", "caramel" => "Salted Caramel Cupcake", "velvet" => "Red Velvet",
+//                "lemon" => "Lemon Drop", "tiramisu" => "Tiramisu");
+//        ?>
+
+
+
+    </form>
+</body>
+</html>
